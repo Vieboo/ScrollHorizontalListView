daae69203da8c858c6c39c85e6981409b028c737
Vieboo <vieboo@hotmail.com>
2016-05-12 16:28:18 +0800

    Initial commit

---
+++ sample/src/main/java/vb/shlv/sample/ListAdapter.java
@@ -0,0 +1,81 @@
+package vb.shlv.sample;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+import vb.shlv.MyHScrollView;
+
+/**
+ * Created by Vieboo on 2016/5/12.
+ */
+public class ListAdapter extends BaseAdapter {
+
+    Context mContext;
+    List<String> dataList;
+    View headView;
+
+    MyHScrollView head_scroll;
+
+    public ListAdapter(Context context, List<String> list, View head) {
+        this.mContext = context;
+        this.dataList = list;
+        this.headView = head;
+        head_scroll = (MyHScrollView) headView.findViewById(R.id.head_scroll);
+    }
+
+    @Override
+    public int getCount() {
+        return dataList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dataList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder = null;
+        if(null == convertView) {
+            holder = new ViewHolder();
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.layout_list_item, null, false);
+            holder.list_scroll = (MyHScrollView) convertView.findViewById(R.id.list_scroll);
+            head_scroll.AddOnScrollChangedListener(new OnScrollChangedListenerImp(holder.list_scroll));
+            convertView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) convertView.getTag();
+        }
+
+        return convertView;
+    }
+
+    /**
+     * 加ViewHolder防止数据多了滑动列表位置错乱
+     */
+    class ViewHolder {
+        MyHScrollView list_scroll;
+    }
+
+    class OnScrollChangedListenerImp implements MyHScrollView.OnScrollChangedListener {
+        MyHScrollView mScrollViewArg;
+
+        public OnScrollChangedListenerImp(MyHScrollView scrollViewar) {
+            mScrollViewArg = scrollViewar;
+        }
+
+        public void onScrollChanged(int l, int t, int oldl, int oldt) {
+            mScrollViewArg.smoothScrollTo(l, t);
+        }
+    }
+}
---
+++ sample/src/main/java/vb/shlv/sample/MainActivity.java
@@ -0,0 +1,101 @@
+package vb.shlv.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private GestureDetector mGesture;
+    // 事件状态
+    private final char FLING_NORMAL = 0;
+    private final char FLING_CLICK = 1;
+    private final char FLING_LEFT = 2;
+    private final char FLING_RIGHT = 3;
+    private char flingState = FLING_NORMAL;
+
+
+    private ListView listview;
+    private LinearLayout head;
+    private ListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mGesture = new GestureDetector(this, mOnGesture);
+
+        head = (LinearLayout) findViewById(R.id.head);
+        head.setFocusable(true);
+        head.setClickable(true);
+        head.setOnTouchListener(new ListViewAndHeadViewTouchLinstener());
+
+        List<String> dataList = new ArrayList<>();
+        int i=0;
+        while(i < 50) {
+            i++;
+            dataList.add("");
+        }
+        adapter = new ListAdapter(this, dataList, head);
+        listview = (ListView) findViewById(R.id.listview);
+        listview.setAdapter(adapter);
+        listview.setOnTouchListener(new ListViewAndHeadViewTouchLinstener());
+    }
+
+    /**
+     * 手势
+     */
+    private GestureDetector.OnGestureListener mOnGesture = new GestureDetector.SimpleOnGestureListener() {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float vX, float vY) {
+            if(e2.getX()-e1.getX()>20){
+                flingState = FLING_LEFT;
+
+            }else if(e1.getX()-e2.getX()>20){
+                flingState = FLING_RIGHT;
+            }
+            return false;
+        }
+
+        /** 滚动 */
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2,float distanceX, float distanceY) {
+            return false;
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            flingState = FLING_CLICK;
+            return false;
+        }
+    };
+
+
+    class ListViewAndHeadViewTouchLinstener implements View.OnTouchListener {
+
+        @Override
+        public boolean onTouch(View arg0, MotionEvent ev) {
+            // 当在列头 和 listView控件上touch时，将这个touch的事件分发给 ScrollView
+            HorizontalScrollView headSrcrollView = (HorizontalScrollView) head.findViewById(R.id.head_scroll);
+            headSrcrollView.onTouchEvent(ev);
+            mGesture.onTouchEvent(ev);
+            return false;
+        }
+
+    }
+}
